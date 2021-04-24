ec4333083f3ef0f3b364d493c62c08cf331d27c6
mitra51097 <37516378+mitra51097@users.noreply.github.com>
2021-04-24 10:46:35 +0530

    Add files via upload

---
+++ server3.js
@@ -0,0 +1,9 @@
+let http =require("http");
+let server = http.createServer((req,res)=>{
+    res.writeHead(200,{"content-type":"text/html"});
+    res.write("<h1>welcome to server3</h1>");
+    res.end();
+});
+server.listen(8080,()=>{
+    console.log("server3 running at port 8084")
+});
